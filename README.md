e0e1502e03bf9fcd8e10080046ffead05da9840c
Marina Brown <galmarpet@google.com>
2019-08-14 12:10:08 +0400

    add task-1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Here are interesting and useful solutions to various small tasks.
+
+> task-1
+###### by clicking on li, '!' is added to its end.
+###### button, by clicking on which a new li with the text 'item' is added to the end of ul.
---
+++ task-1/main.js
@@ -0,0 +1,13 @@
+const ul = document.getElementById('ul');
+
+ul.addEventListener('click', addSign);
+function addSign() {
+	event.target.innerHTML = event.target.innerHTML + '!';
+}
+var button = document.getElementById('button');
+button.addEventListener('click', addLi);
+function addLi() {
+	const li = document.createElement('li');
+	li.innerHTML = 'new li';
+	ul.appendChild(li);
+}
