81bbf0828b31728e388bcb2214f7e59a73b3cf24
Marina Brown <galmarpet@google.com>
2019-08-14 12:19:26 +0400

    add task-2

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 > task-1
 ###### by clicking on li, '!' is added to its end.
 ###### button, by clicking on which a new li with the text 'item' is added to the end of ul.
+
+> task-2
+###### On blur, the sum of the digits of the number from the input.
---
+++ task-2/main.js
@@ -0,0 +1,13 @@
+const num = document.querySelector(".num");
+num.addEventListener('blur', getSum);
+
+function getSum() {
+  const numValue = num.value;
+const sum = document.querySelector(".sum");
+  arr = numValue.split('');
+  let baseSum = 0;
+for(var i = 0; i < arr.length; i++){
+    baseSum += parseInt(arr[i]);
+    }
+  sum.innerHTML = baseSum;
+}
